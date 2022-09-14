4e7c34a2719b76c2ffbb6190eaf99a2bc4d4cb12
raylu15742cs <raylucs15742@gmail.com>
2022-09-14 11:27:40 -0700

    update beluga card

--- portfolio/src/Component/Project.js
+++ portfolio/src/Component/Project.js
@@ -28,6 +28,19 @@ function Project() {
         </Carousel>
         <div className="description beluga">
           <h2> Yeezy Beluga Themed Store</h2>
+          <div className="descriptionInside">
+            <h3>About</h3>
+            <p>This project was created to replicate that of a fully functional eccomerce store. I choose the Yeezy Beluga 350 V2 because of its history and design. This shoe was released in 2016 as the first 350 v2 with that beautiful orange stripe that makes the wearer of this piece stand out </p>
+            <h3>Creation</h3>
+            <p>Developed a React Application, that replicates all of the functionality of a themed eccomerce website. This project was built using routers, hooks, and functions components. It is published on github pages using hashrouters </p>
+            <h3>Technologies</h3>
+            <div className="Technologies">
+              <p>React</p>
+              <p>JavaScript</p>
+              <p>HTML</p>
+              <p>CSS</p>
+            </div>
+          </div>
           <div className="descriptionButtons">
             <button onClick={() => {window.open('https://raylu15742cs.github.io/Beluga-Theme-Store/', '_blank');}}> Live Link &#x2192;</button>
             <button onClick={() => {window.open('https://github.com/raylu15742cs/Beluga-Theme-Store', '_blank');}}> Github Repo &#x2192;</button>
@@ -37,6 +50,16 @@ function Project() {
       <div className="pokemonCarousel card">
         <div className="description Pokemon">
           <h2>Pokemon Memory Card</h2>
+          <h3>About</h3>
+          <p>Fact 1</p>
+          <p> Fact 2</p>
+          <p> Fact 3</p>
+          <h3>Technologies</h3>
+          <div className="Technologies">
+            <p>JavaScript</p>
+            <p>HTML</p>
+            <p>CSS</p>
+          </div>
           <div className="descriptionButtons">
             <button onClick={() => {window.open('https://raylu15742cs.github.io/Pokemon-Memory-Game/', '_blank');}}> Live Link &#x2192;</button>
             <button onClick={() => {window.open('https://github.com/raylu15742cs/Pokemon-Memory-Game', '_blank');}}> Github Repo &#x2192;</button>
@@ -68,6 +91,16 @@ function Project() {
         </Carousel>
         <div className="description battleship">
           <h2>Battleship</h2>
+          <h3>About</h3>
+          <p>Fact 1</p>
+          <p> Fact 2</p>
+          <p> Fact 3</p>
+          <h3>Technologies</h3>
+          <div className="Technologies">
+            <p>JavaScript</p>
+            <p>HTML</p>
+            <p>CSS</p>
+          </div>
           <div className="descriptionButtons">
             <button onClick={() => {window.open('https://raylu15742cs.github.io/Battleship-Game/', '_blank');}}> Live Link &#x2192;</button>
             <button onClick={() => {window.open('https://github.com/raylu15742cs/Battleship-Game', '_blank');}}> Github Repo &#x2192;</button>
