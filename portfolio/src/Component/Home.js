1538c763ccf5bd631e09803748e5298ee237f20b
raylu15742cs <raylucs15742@gmail.com>
2022-09-15 10:38:06 -0700

    remove skills

--- portfolio/src/Component/Home.js
+++ portfolio/src/Component/Home.js
@@ -9,32 +9,18 @@ function Home() {
           <h2> Hello I'm Raymond Lu </h2>
           <h3>I am a front end web developer based in Pasadena, California</h3>
           <div className="HomeButtons">
-            <Link to="/project"><button>Projects</button></Link>
-            <button><a href={resume} target="#" >Resume</a></button>
+            <Link to="/project">
+              <button>Projects</button>
+            </Link>
+            <button>
+              <a href={resume} target="#">
+                Resume
+              </a>
+            </button>
           </div>
         </div>
         <img src={profile} alt="profile"></img>
       </div>
-      {/*
-      <div className="skills">
-        <h4>Language</h4>
-        <p>Javascript</p>
-        <h4>Frameworks</h4>
-        <p>React</p>
-        <h4>Web Development</h4>
-        <p>HTML , CSS , SCSS/SASS</p>
-        <h4>Automated Testing</h4>
-        <p>Jest, React Testing Library</p>
-        <h4>Debugging</h4>
-        <p>Debugger , Console</p>
-        <h4>APIs</h4>
-        <h4>Version Control System</h4>
-        <p>Git , Github</p>
-        <h4>Command Line / OS</h4>
-        <p>Mac , Windows</p>
-        <h4>Optimization</h4>
-      </div>
-  */}
     </div>
   );
 }
